d836074be96cf7054ccd4b15cddd3ccb09ad3389
LEANDRO DEL SUELDO LANDA <leandro.del_sueldo_landa@open-groupe.com>
2022-03-08 22:30:24 +0100

    Termina 53

--- Peliculas/Peliculas/Shared/Entidades/Genero.cs
+++ Peliculas/Peliculas/Shared/Entidades/Genero.cs
@@ -1,8 +1,12 @@
-﻿namespace Peliculas.Shared.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Peliculas.Shared.Entidades
 {
     public class Genero
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Nombre { get; set; }
     }
 }
