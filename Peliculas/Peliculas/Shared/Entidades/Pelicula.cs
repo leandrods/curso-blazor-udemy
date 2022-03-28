874a217fefeb9def1e713cb929c93df62dd474cf
LEANDRO DEL SUELDO LANDA <leandro.del_sueldo_landa@open-groupe.com>
2022-03-28 14:44:07 +0200

    Fin de seccion 5, pero no escribí el código de los videos 60 ni 61

--- Peliculas/Peliculas/Shared/Entidades/Pelicula.cs
+++ Peliculas/Peliculas/Shared/Entidades/Pelicula.cs
@@ -1,9 +1,23 @@
-﻿namespace Peliculas.Shared.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Peliculas.Shared.Entidades
 {
     public class Pelicula
     {
+        public int Id { get; set; }
+
+        [Required]
         public string Titulo { get; set; } = "";
-        public DateOnly Lanzamiento { get; set; }
+
+        public string Resumen { get; set; }
+
+        public bool EnCartelera { get; set; }
+
+        public string Trailer { get; set; }
+
+        [Required]
+        public DateOnly? Lanzamiento { get; set; }
+
         public string Poster { get; set; }
 
         public string TituloCortado
